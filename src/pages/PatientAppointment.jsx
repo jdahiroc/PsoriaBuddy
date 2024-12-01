13f0d3935bca9bbb186e029dc35d46f79c1c9e33
jdahiroc <jdahiroc_210000000608@uic.edu.ph>
2024-12-01 18:29:33 +0800

    Remove the isFormComplete for selectedExperience

--- src/pages/PatientAppointment.jsx
+++ src/pages/PatientAppointment.jsx
@@ -183,7 +183,6 @@ const PatientAppointment = () => {
       patientContact &&
       selectedGender &&
       selectedService &&
-      selectedExperience &&
       preferredDay &&
       preferredTime
     );
