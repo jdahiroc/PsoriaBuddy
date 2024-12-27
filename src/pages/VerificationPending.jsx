a332a81f31bf2a9da6bf8faf6fafe9777f5b9c10
jdahiroc <jdahiroc_210000000608@uic.edu.ph>
2024-12-28 04:11:56 +0800

    Improved the design & changed the navigation link for button

--- src/pages/VerificationPending.jsx
+++ src/pages/VerificationPending.jsx
@@ -1,20 +1,51 @@
-import { Button, Result } from "antd";
+import { useEffect, useContext } from "react";
+import { useNavigate } from "react-router-dom";
 import useLogout from "../context/useLogout";
+import { AuthContext } from "../context/AuthContext";
+
+import { Button, Result } from "antd";
+
+import ListItemIcon from "@mui/material/ListItemIcon";
+import Logout from "@mui/icons-material/Logout";
+import { Box } from "@mui/material";
 
 const VerificationPending = () => {
-  // Initialize the custom logout hook
-  const { logout } = useLogout();
+  // Initialize verification from auth context
+  const { verification } = useContext(AuthContext);
+  // Initialize the useNavigate
+  const navigate = useNavigate();
+
+  // goToProfile Function
+  const goToProfile = () => {
+    navigate("/d/profile");
+  };
+
+  // Checks if user verification status
+  useEffect(() => {
+    if (verification === "verified") {
+      // Redirect to the dermatologist profile
+      navigate("/d/profile", { replace: true });
+    }
+  }, [verification, navigate]);
 
   return (
-    <Result
-      title="Your account is under verification."
-      subTitle="We will notify you once your account is verified. Please try again later."
-      extra={
-        <Button type="primary" onClick={logout}>
-          Back to login
-        </Button>
-      }
-    />
+    <>
+      <Result
+        status="403"
+        title="Your account is under verification."
+        subTitle="Your documents have been submitted. Please allow 1 to 5 business days for processing."
+        extra={
+          <Button
+            type="primary"
+            size="large"
+            style={{ width: "620px" }}
+            onClick={goToProfile}
+          >
+            Yes, I understand.
+          </Button>
+        }
+      />
+    </>
   );
 };
 
